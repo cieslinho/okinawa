49c8004517b51f18b360a7b154a46011ad52b251
Szymon Cieśla <cszymon01@gmail.com>
2023-11-17 19:36:33 +0100

    created file structure

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Ciesla Szymon
+ */
+?>
+<footer class="footer">
+
+    
+</footer>
+<?php wp_footer(); ?>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Okinawa
+ */
+get_header();
+?>
+
+
+<main class="main">
+<?php the_content(); ?>
+
+</main>
+
+<?php
+get_footer()
+?>
